4efa01d0a7ca8daa35a7eee7ced117c6fb687291
Rob Spears <rob.sp3ars@gmail.com>
2020-05-13 15:30:29 -0400

    Create Post model

--- ghost_post/models.py
+++ ghost_post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    is_roast = models.BooleanField()
+    content = models.CharField(max_length=280)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.content
