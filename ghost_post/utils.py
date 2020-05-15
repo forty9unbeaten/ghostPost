3e57254ffb4a874a9045dcb2d69f652d77c8129d
Rob Spears <rob.sp3ars@gmail.com>
2020-05-14 23:39:29 -0400

    write function to generate a secret_id for each Post created

--- ghost_post/utils.py
+++ ghost_post/utils.py
@@ -1,5 +1,32 @@
+from string import ascii_lowercase
+from random import choice
+
 post_filters = [
-    'All',
-    'Boasts',
-    'Roasts'
+    {
+        'value': 'all',
+        'display': 'All'
+    },
+    {
+        'value': 'boasts',
+        'display': 'Boasts'
+    },
+    {
+        'value': 'roasts',
+        'display': 'Roasts'
+    }
 ]
+
+post_sorters = [
+    {
+        'value': 'created',
+        'display': 'Creation Date'
+    },
+    {
+        'value': 'score',
+        'display': 'Vote Score/Total'
+    }
+]
+
+
+def generate_secret_id():
+    return ''.join(choice(ascii_lowercase) for i in range(6))
