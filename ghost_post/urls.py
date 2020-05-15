dc3cdcfc4250a665ef4ba00f851e3f711f5a5ccf
Rob Spears <rob.sp3ars@gmail.com>
2020-05-14 23:40:29 -0400

    Add url to render template that adds new post and template to show details of post

--- ghost_post/urls.py
+++ ghost_post/urls.py
@@ -3,6 +3,9 @@ from ghost_post import views
 
 url_paths = [
     path('', views.landing_view, name='home_landing'),
-    path('<str:post_filter>/', views.homepage_view, name='homepage'),
-    path('<str:post_filter>/<str:action>/<int:post_id>', views.post_vote_view)
+    path('new/', views.new_post_view, name='new_post'),
+    path('<str:post_id>', views.post_detail_view, name='details'),
+    path('<int:post_id>/<str:action>', views.post_vote_view),
+    path('<str:post_filter>/<str:post_sort>',
+         views.homepage_view, name='homepage'),
 ]
